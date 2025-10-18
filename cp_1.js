751371dfee601f8e779ec8bb8d048781aa37463e
Isabella Miranda <ifmiranda@usf.edu>
2025-10-17 21:33:53 -0400

    Added character count

--- cp_1.js
+++ cp_1.js
@@ -5,3 +5,8 @@ const display = document.getElementById("feedback-display");
 const tooltip = document.getElementById("tooltip");
 const count = document.getElementById("char-count");
 
+// Character counter 
+form.addEventListener("input", e=> {
+    if (e.target.id === "comments") count.textContent = e.target.value.length;
+
+}); 
