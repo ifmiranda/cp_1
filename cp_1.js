065e8ca687efd7ef88e58a2ebaa96d52b3378727
Isabella Miranda <ifmiranda@usf.edu>
2025-10-17 21:31:22 -0400

    DOM references

---
+++ cp_1.js
@@ -0,0 +1,7 @@
+// DOM references 
+
+const form = document.getElementById("feedback-form");
+const display = document.getElementById("feedback-display");
+const tooltip = document.getElementById("tooltip");
+const count = document.getElementById("char-count");
+
