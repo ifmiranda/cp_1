1efd33eab0c8ff95b15251085a0c7eb676aff9ef
Isabella Miranda <ifmiranda@usf.edu>
2025-10-19 14:14:44 -0400

    Cleand up html interface

--- cp_1.js
+++ cp_1.js
@@ -10,3 +10,34 @@ form.addEventListener("input", e=> {
     if (e.target.id === "comments") count.textContent = e.target.value.length;
 
 }); 
+// Tooltips 
+form.addEventListener("mouseover", e => {
+    if (e.target.matches("input, textarea")) {
+        tooltip.textContent = e.target.dataset.tip;
+        tooltip.hidden = false;
+    }
+}); 
+form.addEventListener("mouseout", () => tooltip.hidden = true); 
+
+// Stopping background click events 
+form.addEventListener("click", e => e.stopPropagation());
+document.addEventListener("click", () => tooltip.hidden = true); 
+
+// Submit event with simple validation 
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const name = form.name.value.trim();
+    const email = form.email.value.trim();
+    const comments = form.comments.value.trim(); 
+
+    if (!name || !email || !comments) {
+        alert ("Please fill out all required fiels!");
+        return; 
+    }
+
+    const card = document.createElement("div");
+    card.textContent = `${name} (${email}): ${comments}`;
+    display.appendChild(card);
+    form.requestFullscreen();
+    count.textContent = 0
+}); 
